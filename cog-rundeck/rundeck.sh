22ae44e006f2a6d655ded14459cf0ba70063c36e
Stephen Price <stephen@stp5.net>
2017-02-02 10:04:03 -0800

    add more commands

--- cog-rundeck/rundeck.sh
+++ cog-rundeck/rundeck.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
+export PATH=${PATH}:/usr/local/bin
+
 declare -a ARGUMENTS
 for ((i=0;i<${COG_ARGC};i++)); do
     var="COG_ARGV_${i}"
     ARGUMENTS[$i]=${!var}
 done
 
+if [ -n "${COG_OPT_PRJ}" ]; then
+  project=${COG_OPT_PRJ}
+else
+  project=${RUNDECK_DEFAULT_PROJECT}
+fi
+
 case "$1" in
   list)
-    command=rundeck-list-jobs
+    rundeck-list-jobs ${project} ${ARGUMENTS[*]}
     ;;
   run)
-    command=rundeck-run-job
+    jobid=$(rundeck-find-job-by-name "${ARGUMENTS[*]}" ${project} | grep " ${ARGUMENTS[*]} " | cut -d' ' -f2)
+    if [ -n "${jobid}" ]; then
+      rundeck-run-job ${jobid} ${COG_OPT_ARG}
+    else
+      echo "ERROR: job not found."
+    fi
     ;;
   history)
-    command=rundeck-get-history
+    rundeck-get-history ${project}
     ;;
   *)
     echo "ERROR: unknown command"
     exit 1
 esac
-
-/usr/local/bin/${command} ${ARGUMENTS[*]}
